ec8de2859cc40e18561f7d3d8176baa03b248909
maximkfd <ipust13@gmail.com>
2017-03-31 12:15:51 +0300

    Initial commit

---
+++ src/main/java/com/repp/AvitifierApplication.java
@@ -0,0 +1,12 @@
+package com.repp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvitifierApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AvitifierApplication.class, args);
+	}
+}
